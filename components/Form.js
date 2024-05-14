bfd90e33755cf9311ec4e28130b2ef26d1764c22
Sahil Sinha <115421224+SahilSinha22@users.noreply.github.com>
2024-05-14 15:12:26 +0530

    Update Form.js

--- components/Form.js
+++ components/Form.js
@@ -184,7 +184,7 @@ const Form = () => {
             <button
               className="text-white border-white border-2  bg-black hover:bg-zinc-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Get a free quote -
+              Get a free quote →
             </button>
           </form>
         </div>
