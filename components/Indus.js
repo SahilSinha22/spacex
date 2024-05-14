f261e5825a7096a8ec5240e76c3daf868755c747
Sahil Sinha <115421224+SahilSinha22@users.noreply.github.com>
2024-05-15 01:08:19 +0530

    Update Indus.js

--- components/Indus.js
+++ components/Indus.js
@@ -1,106 +1,158 @@
-import React from 'react'
-import Image from 'next/image'
-import Doc from "@/public/doctor.jpg"
+import React from "react";
+import Image from "next/image";
+import Doc from "@/public/doctor.jpg";
 const Indus = () => {
   return (
     <div className=" max-w-7xl  px-4 sm:px-6 lg:px-8 py-12">
-        <div className="text-left mx-10 md:mx-20">
-            <h2 className="text-3xl font-bold font-sedan md:text-6xl text-zinc-900 dark:text-white">Industries we serve</h2>
-            <p className="mt-4 text-lg md:text-2xl text-zinc-600 dark:text-zinc-300">
-                Our top mobile app development company proudly serves a diverse range of industries. Through
-                innovative technology solutions, we empower businesses in numerous sectors to engage customers,
-                optimize operations, and unlock new opportunities for growth.
+      <div className="text-left mx-10 md:mx-20">
+        <h2 className="text-3xl font-bold font-sedan md:text-6xl text-zinc-900 dark:text-white">
+          Industries we serve
+        </h2>
+        <p className="mt-2 text-lg md:text-2xl text-zinc-600 dark:text-zinc-300">
+          Our top mobile app development company proudly serves a diverse range
+          of industries. Through innovative technology solutions, we empower
+          businesses in numerous sectors to engage customers, optimize
+          operations, and unlock new opportunities for growth.
+        </p>
+      </div>
+      <div className="relative h-96 sm:h-60 md:h-60 lg:h-80  mt-10 mx-10 sm:mx-10 md:mx-20 lg:ml-20 justify-left  grid grid-cols-3  sm:grid-cols-6   md:grid-cols-6 ">
+       
+        <div className=" relative md:h-60 lg:h-80    xl:w-72 lg:hover:h-80 bg-black sm:h-56  dark:bg-zinc-800  hover:w-10 sm:hover:w-16 h-48 md:hover:h-auto md:hover:w-20 lg:hover:w-24 md:w-60 lg:w-64 xl:hover:w-28  w-52  p-2  rounded-lg">
+          <div className="  absolute z-10  bottom-1 md:bottom-4  hover:opacity-0 p-4 opacity-100">
+            <h3 className="mt-2 lg:text-lg text-sm font-semibold text-white  dark:text-white">
+              Healthcare
+            </h3>
+            <div className="mt-1 md:mt-2">
+            <p className="text-white  text-xs  lg:text-sm  dark:text-zinc-300">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's.
             </p>
+            </div>
+            <div className="mt-1 md:mt-4">
+            <a
+              href="#"
+              className="  lg:px-3 lg:py-2  text-xs lg:text-sm border-2 border-solid px-2 border-white rounded-full   text-white dark:text-blue-400 "
+            >
+              Explore more →
+            </a>
+            </div>
+          </div>
         </div>
-        <div className="relative h-80 mt-10 mx-10 sm:mx-10 md:mx-20 lg:ml-20 justify-left  grid grid-cols-4  sm:grid-cols-6   md:grid-cols-6 ">
-            <div className=" relative  bg-black  dark:bg-zinc-800  w-6 sm:w-16 hover:h-80 md:hover:h-auto md:w-20 lg:w-24 md:hover:w-60 lg:hover:w-80  hover:w-52  p-6  rounded-lg"
-            >
-            <div className="  absolute z-10  bottom-10  opacity-0 p-6 hover:opacity-100">
-                <h3 className="mt-4  text-sm font-semibold text-white  dark:text-white">Healthcare</h3>
-                <p className="text-white text-xs  dark:text-zinc-300">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's.
-                </p>
-                <a href="#" className=" text-xs border-2 border-solid px-2 border-white rounded-full  mt-4 text-white dark:text-blue-400 ">
-                    Explore more →
-                </a>
-                </div>
-            </div>
-            <div className="relative xl:-left-[74px]  lg:-left-8  md:-left-2 -left-3 sm:-left-5 bg-black dark:bg-zinc-800 mr-20 w-6 sm:w-16 hover:h-80 md:hover:h-auto  md:w-20 lg:w-24 md:hover:w-60 lg:hover:w-80  hover:w-52 p-6 rounded-lg"
-            >
-                <div className=" absolute z-10 bottom-10 opacity-0 hover:opacity-100">
 
-                <h3 className="mt-4 text-sm font-semibold text-white dark:text-white">Finance</h3>
-                <p className="text-white text-xs  dark:text-zinc-300">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's.
-                </p>
-                <a href="#" className=" border-2 text-xs border-solid px-2 border-white rounded-full  mt-4 text-white dark:text-blue-400">
-                    Explore more →
-                </a>
+        <div className=" relative left-32 md:h-60 lg:left-32 lg:h-80 xl:left-32 xl:hover:w-72 lg:hover:h-80 md:left-[150px] sm:left-[120px] bg-black sm:h-56  dark:bg-zinc-800 hover:w-40 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24  xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
+          <div className="  absolute z-10  bottom-2  opacity-0 p-4 hover:opacity-100">
+            <h3 className="mt-2  text-sm lg:text-lg font-semibold text-white  dark:text-white">
+              Healthcare
+            </h3>
+            <div className="mt-1 md:mt-2  ">
+            <p className="text-white  text-xs lg:text-sm  dark:text-zinc-300">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's.
+            </p>
             </div>
-            </div>
-            <div className="relative xl:-left-[145px] lg:-left-16 md:-left-4 -left-6 sm:-left-10  bg-black dark:bg-zinc-800 w-6 sm:w-16   hover:h-80 md:hover:h-auto  md:w-20 lg:w-24 md:hover:w-60 lg:hover:w-80  hover:w-52 p-6 rounded-lg"
+            <div className="mt-2 md:mt-4 ">
+            <a
+              href="#"
+              className="  lg:px-3 lg:py-2  text-xs lg:text-sm border-2 border-solid px-2 border-white rounded-full   text-white dark:text-blue-400 "
             >
-                <div className=" absolute z-10 bottom-10 opacity-0 hover:opacity-100">
-
-                <h3 className="mt-4 text-sm font-semibold text-white dark:text-white">Retail</h3>
-                <p className="text-white text-xs  dark:text-zinc-300">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's.
-                </p>
-                <a href="#" className="text-xs border-2 border-solid px-2 border-white rounded-full  mt-4 text-white dark:text-blue-400">
-                    Explore more →
-                </a>
+              Explore more →
+            </a>
             </div>
-            </div>
-            <div className="relative xl:-left-[220px] lg:-left-24 md:-left-6 -left-8 sm:-left-[60px] bg-black dark:bg-zinc-800 w-6 sm:w-16 hover:h-80 md:hover:h-auto  md:w-20 lg:w-24 md:hover:w-60 lg:hover:w-80  hover:w-52 p-6 rounded-lg"
-            >
-                <div className="z-10 absolute bottom-10 opacity-0 hover:opacity-100">
-
-                <h3 className="mt-4 text-sm font-semibold text-white dark:text-white">Education</h3>
-                <p className="text-xs text-white  dark:text-zinc-300">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's.
-                </p>
-                <a href="#" className="border-2 text-xs border-solid px-2 border-white rounded-full  mt-4 text-white dark:text-blue-400">
-                    Explore more →
-                </a>
-            </div>
-            </div>
-            <div className="relative mt-2 xl:-left-[291px] sm:mt-0 lg:-left-32 md:-left-8  sm:-left-20 bg-black dark:bg-zinc-800 w-6 sm:w-16  hover:h-80 md:hover:h-auto  md:w-20 lg:w-24 md:hover:w-60 lg:hover:w-80  hover:w-52 p-6 rounded-lg"
-            >
-                <div className="z-10 absolute bottom-10 opacity-0 hover:opacity-100">
-
-                <h3 className="mt-4 text-sm font-semibold text-white dark:text-white">Education</h3>
-                <p className="text-white text-xs  dark:text-zinc-300">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's.
-                </p>
-                <a href="#" className="border-2 text-xs border-solid px-2 border-white rounded-full  mt-4 text-white dark:text-blue-400">
-                    Explore more →
-                </a>
-            </div>
-            </div>
-            <div className="relative  mt-2 xl:-left-[364px] sm:mt-0 lg:-left-40 md:-left-10 -left-2 sm:-left-24 bg-black dark:bg-zinc-800 w-6 sm:w-16 hover:h-80 md:hover:h-auto  md:w-20 lg:w-24 md:hover:w-60 lg:hover:w-80  hover:w-52 p-6 rounded-lg"
-            >
-                <div className="z-10 absolute bottom-10 opacity-0 hover:opacity-100">
-
-                <h3 className="mt-4 text-sm font-semibold text-white dark:text-white">Education</h3>
-                <p className="text-white text-xs dark:text-zinc-300">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's.
-                </p>
-                <a href="#" className="border-2 text-xs border-solid px-2 border-white rounded-full  mt-4 text-white dark:text-blue-400">
-                    Explore more →
-                </a>
-            </div>
-            </div>
+          </div>
         </div>
+
+        <div className=" relative left-24 md:h-60 lg:left-24 lg:h-80 xl:left-20 xl:hover:w-72 lg:hover:h-80 md:left-32 sm:left-[94px] bg-black sm:h-56  dark:bg-zinc-800 hover:w-40 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24 xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
+          <div className="  absolute z-10  bottom-2  opacity-0 p-4 hover:opacity-100">
+            <h3 className="mt-2  text-sm lg:text-lg font-semibold text-white  dark:text-white">
+              Healthcare
+            </h3>
+            <div className="mt-1 md:mt-2">
+            <p className="text-white  text-xs lg:text-sm  dark:text-zinc-300">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's.
+            </p>
+            </div>
+            <div className="mt-2 md:mt-4 ">
+            <a
+              href="#"
+              className="  lg:px-3 lg:py-2  text-xs lg:text-sm border-2 border-solid px-2 border-white rounded-full   text-white dark:text-blue-400 "
+            >
+              Explore more →
+            </a>
+            </div>
+          </div>
+        </div>
+
+        <div className=" relative mt-1  md:h-60 lg:left-[68px] lg:h-80 xl:left-6 xl:hover:w-72 lg:hover:h-80 md:left-28 sm:mt-0 sm:left-[74px] bg-black sm:h-56  dark:bg-zinc-800 hover:w-40 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24 xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
+          <div className="  absolute z-10  bottom-2  opacity-0 p-4 hover:opacity-100">
+            <h3 className="mt-2  text-sm lg:text-lg font-semibold text-white  dark:text-white">
+              Healthcare
+            </h3>
+            <div className="mt-1 md:mt-2">
+            <p className="text-white  text-xs lg:text-sm  dark:text-zinc-300">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's.
+            </p>
+            </div>
+            <div className="mt-2 md:mt-4">
+            <a
+              href="#"
+              className="  lg:px-3 lg:py-2  text-xs lg:text-sm border-2 border-solid px-2 border-white rounded-full   text-white dark:text-blue-400 "
+            >
+              Explore more →
+            </a>
+            </div>
+          </div>
+        </div>
+
+        <div className=" relative mt-1 md:h-60 lg:h-80 xl:-left-8 xl:hover:w-72 lg:left-10 lg:hover:h-80 md:left-24 sm:mt-0 sm:left-[60px] -left-10 sm:h-56 bg-black  dark:bg-zinc-800 hover:w-40 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24 xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
+          <div className="  absolute z-10  bottom-2  opacity-0 p-4 hover:opacity-100">
+            <h3 className="mt-2  text-sm lg:text-lg font-semibold text-white  dark:text-white">
+              Healthcare
+            </h3>
+            <div className="mt-1 md:mt-2">
+            <p className="text-white  text-xs lg:text-sm  dark:text-zinc-300">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's.
+            </p>
+            </div>
+            <div className="mt-2 md:mt-4 ">
+            <a
+              href="#"
+              className="  lg:px-3 lg:py-2  text-xs lg:text-sm border-2 border-solid px-2 border-white rounded-full   text-white dark:text-blue-400 "
+            >
+              Explore more →
+            </a>
+            </div>
+          </div>
+        </div>
+
+        <div className=" relative md:h-60 lg:h-80 xl:-left-20 xl:hover:w-72 lg:left-3 lg:hover:h-80 -left-20 md:left-20 sm:mt-0 sm:left-10  sm:h-56 mt-1 bg-black  dark:bg-zinc-800 hover:w-40 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24 xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
+          <div className="  absolute z-10  bottom-2  opacity-0 p-4 hover:opacity-100">
+            <h3 className="mt-2  text-sm lg:text-lg font-semibold text-white  dark:text-white">
+              Healthcare
+            </h3>
+            <div className="mt-1 md:mt-2">
+            <p className="text-white  text-xs lg:text-sm  dark:text-zinc-300">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's.
+            </p>
+            </div>
+            <div className="mt-2 md:mt-4 ">
+            <a
+              href="#"
+              className="  lg:px-3 lg:py-2  text-xs lg:text-sm border-2 border-solid px-2 border-white rounded-full   text-white dark:text-blue-400 "
+            >
+              Explore more →
+            </a>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
     </div>
+  );
+};
 
-
-  )
-}
-
-export default Indus
+export default Indus;
