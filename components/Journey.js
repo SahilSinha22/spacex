4521808eee248b25ff0b2e255b383f7be06dd1a2
Sahil Sinha <115421224+SahilSinha22@users.noreply.github.com>
2024-05-14 13:57:27 +0530

    Update Journey.js

--- components/Journey.js
+++ components/Journey.js
@@ -15,10 +15,10 @@ const Journey = () => {
         </p>
         <div className="grid grid-cols-1 sm:grid-cols-2   space-y-5 lg:grid-cols-4 xl:grid-cols-5  justify-center items-center space-x-8 mt-8">
           <div className="text-center   mt-4 ml-10">
-            <div className="w-48 h-48 border-dashed mb-4 border-2  mx-auto  flex items-center justify-center rounded-full">
-              <div className="relative w-40 h-40  bg-gray-200 rounded-full items-center">
+            <div className="w-48 h-48 2xl:w-52 2xl:h-52  border-dashed mb-4 border-2  mx-auto  flex items-center justify-center rounded-full">
+              <div className="relative 2xl:w-48 2xl:h-48 w-40 h-40    bg-gray-200 rounded-full items-center">
                 <Image
-                  className="absolute top-8 left-6"
+                  className="absolute top-8 left-6 2xl:top-12 2xl:left-10"
                   src="book.svg"
                   alt="Inception"
                   width={100}
@@ -28,11 +28,11 @@ const Journey = () => {
             </div>
             <div className="text-sm md:text-xl font-bold">Inception</div>
           </div>
-          <div className="text-center lg:mx-2">
-            <div className="w-48 h-48 border-dashed border-2  mx-auto mb-2 flex items-center justify-center  rounded-full">
-              <div className="relative w-40 h-40  bg-gray-200 rounded-full items-center">
+          <div className="text-center lg:mx-2  ">
+            <div className="w-48 h-48 2xl:mx-6   2xl:w-52 2xl:h-52 border-dashed border-2  mx-auto mb-2 flex items-center justify-center  rounded-full">
+              <div className="relative   2xl:w-48 2xl:h-48 w-40 h-40    bg-gray-200 rounded-full items-center">
                 <Image
-                  className="absolute top-8 left-6"
+                  className="absolute top-8 left-6 2xl:top-12 2xl:left-10"
                   src="design.svg"
                   alt="Design"
                   width={100}
@@ -43,10 +43,10 @@ const Journey = () => {
             <div className="text-sm md:text-xl font-bold">Design</div>
           </div>
           <div className="text-center lg:mx-2">
-            <div className="w-48 h-48 border-dashed border-2 mx-auto mb-2 flex items-center justify-center  rounded-full">
-              <div className="relative w-40 h-40  bg-gray-200 rounded-full items-center">
+            <div className="w-48 h-48 2xl:mx-6 2xl:w-52 2xl:h-52  border-dashed border-2 mx-auto mb-2 flex items-center justify-center  rounded-full">
+              <div className="relative 2xl:w-48 2xl:h-48 w-40 h-40    bg-gray-200 rounded-full items-center">
                 <Image
-                  className="absolute top-8 left-6"
+                  className="absolute top-8 left-6 2xl:top-12 2xl:left-10"
                   src="development.svg"
                   alt="Development"
                   width={100}
@@ -57,10 +57,10 @@ const Journey = () => {
             <div className="text-sm md:text-xl font-bold">Development</div>
           </div>
           <div className="text-center lg:mx-2">
-            <div className="w-48 h-48 border-dashed border-2 mx-auto mb-2 flex items-center justify-center  rounded-full">
-              <div className="relative w-40 h-40  bg-gray-200 rounded-full items-center">
+            <div className="w-48 h-48 2xl:mx-6 2xl:w-52 2xl:h-52  border-dashed border-2 mx-auto mb-2 flex items-center justify-center  rounded-full">
+              <div className="relative 2xl:w-48 2xl:h-48 w-40 h-40    bg-gray-200 rounded-full items-center">
                 <Image
-                  className="absolute top-8 left-6"
+                  className="absolute top-8 left-6 2xl:top-12 2xl:left-10"
                   src="stabi.svg"
                   alt="Stabilization"
                   width={100}
@@ -71,10 +71,10 @@ const Journey = () => {
             <div className="text-sm md:text-xl font-bold">Stabilization</div>
           </div>
           <div className="text-center lg:mx-2">
-            <div className="w-48 h-48 border-dashed border-2 mx-auto mb-2 flex items-center justify-center  rounded-full">
-              <div className="relative  w-40 h-40  bg-gray-200 rounded-full items-center">
+            <div className="w-48 h-48 2xl:mx-6 2xl:w-52 2xl:h-52  border-dashed border-2 mx-auto mb-2 flex items-center justify-center  rounded-full">
+              <div className="relative  2xl:w-48 2xl:h-48 w-40 h-40    bg-gray-200 rounded-full items-center">
                 <Image
-                  className="absolute top-8 left-6 "
+                  className="absolute top-8 left-6 2xl:top-12 2xl:left-10"
                   src="deploy.svg"
                   alt="Deployment"
                   width={100}
@@ -91,3 +91,4 @@ const Journey = () => {
 };
 
 export default Journey;
+
