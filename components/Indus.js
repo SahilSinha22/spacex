3b4e4ef8b0c71ca1f1b57d739f2c81f730878c2b
Sahil Sinha <115421224+SahilSinha22@users.noreply.github.com>
2024-05-15 14:13:13 +0530

    Update Indus.js

--- components/Indus.js
+++ components/Indus.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 import Doc from "@/public/doctor.jpg";
 const Indus = () => {
   return (
-    <div className=" max-w-7xl  px-4 sm:px-6 lg:px-8 py-12">
-      <div className="text-left mx-10 md:mx-20">
+    <div className=" max-w-7xl   lg:px-8 py-12">
+      <div className="text-left ml-10 md:mx-20">
         <h2 className="text-3xl font-bold font-sedan md:text-6xl text-zinc-900 dark:text-white">
           Industries we serve
         </h2>
@@ -15,10 +15,10 @@ const Indus = () => {
           operations, and unlock new opportunities for growth.
         </p>
       </div>
-      <div className="relative h-96 sm:h-60 md:h-60 lg:h-80  mt-10 mx-10 sm:mx-10 md:mx-20 lg:ml-20 justify-left  grid grid-cols-3  sm:grid-cols-6   md:grid-cols-6 ">
+      <div className="relative h-auto sm:h-60 md:h-60 lg:h-80  mt-10 mx-10 sm:mx-10 md:mx-20 lg:ml-20 justify-left  grid grid-cols-2  sm:grid-cols-6   md:grid-cols-6 ">
        
         <div className=" relative md:h-60 lg:h-80    xl:w-72 lg:hover:h-80 bg-black sm:h-56  dark:bg-zinc-800  hover:w-10 sm:hover:w-16 h-48 md:hover:h-auto md:hover:w-20 lg:hover:w-24 md:w-60 lg:w-64 xl:hover:w-28  w-52  p-2  rounded-lg">
-          <div className="  absolute z-10  bottom-1 md:bottom-4  hover:opacity-0 p-4 opacity-100">
+          <div className="  absolute   bottom-1 md:bottom-4  hover:opacity-0 p-4 opacity-100">
             <h3 className="mt-2 lg:text-lg text-sm font-semibold text-white  dark:text-white">
               Healthcare
             </h3>
@@ -39,8 +39,8 @@ const Indus = () => {
           </div>
         </div>
 
-        <div className=" relative left-32 md:h-60 lg:left-32 lg:h-80 xl:left-32 xl:hover:w-72 lg:hover:h-80 md:left-[150px] sm:left-[120px] bg-black sm:h-56  dark:bg-zinc-800 hover:w-40 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24  xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
-          <div className="  absolute z-10  bottom-2  opacity-0 p-4 hover:opacity-100">
+        <div className=" relative sm:hover:w-52 sm:hover:-left-10 hover:-left-20 left-16 md:h-60 lg:left-32 lg:h-80 xl:left-32 xl:hover:w-72 lg:hover:h-80 md:left-[150px] sm:left-[120px] bg-black sm:h-56  dark:bg-zinc-800 hover:w-64 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24  xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
+          <div className="  absolute   bottom-2  opacity-0 p-4 hover:opacity-100">
             <h3 className="mt-2  text-sm lg:text-lg font-semibold text-white  dark:text-white">
               Healthcare
             </h3>
@@ -61,9 +61,9 @@ const Indus = () => {
           </div>
         </div>
 
-        <div className=" relative left-24 md:h-60 lg:left-24 lg:h-80 xl:left-20 xl:hover:w-72 lg:hover:h-80 md:left-32 sm:left-[94px] bg-black sm:h-56  dark:bg-zinc-800 hover:w-40 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24 xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
-          <div className="  absolute z-10  bottom-2  opacity-0 p-4 hover:opacity-100">
-            <h3 className="mt-2  text-sm lg:text-lg font-semibold text-white  dark:text-white">
+        <div className=" relative mt-1 xl:hover:-left-20 sm:hover:w-52 sm:mt-0 z-10 sm:hover:-left-10 left-0 md:h-60 lg:left-24 lg:h-80 xl:left-20 xl:hover:w-72 lg:hover:h-80 md:left-32 sm:left-[94px] bg-black sm:h-56  dark:bg-zinc-800 hover:w-52 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24 xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
+          <div className=" absolute   bottom-2 text-white opacity-0 p-4 hover:opacity-100">
+            <h3 className="mt-2  text-sm lg:text-lg  font-semibold text-white  dark:text-white">
               Healthcare
             </h3>
             <div className="mt-1 md:mt-2">
@@ -83,7 +83,7 @@ const Indus = () => {
           </div>
         </div>
 
-        <div className=" relative mt-1  md:h-60 lg:left-[68px] lg:h-80 xl:left-6 xl:hover:w-72 lg:hover:h-80 md:left-28 sm:mt-0 sm:left-[74px] bg-black sm:h-56  dark:bg-zinc-800 hover:w-40 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24 xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
+        <div className=" relative xl:hover:-left-40  sm:hover:-left-20 sm:hover:w-52 mt-1 -left-24  md:h-60 lg:left-[68px] lg:h-80 xl:left-6 xl:hover:w-72 lg:hover:h-80 md:left-28 sm:mt-0 sm:left-[74px] bg-black sm:h-56  dark:bg-zinc-800 hover:w-52 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24 xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
           <div className="  absolute z-10  bottom-2  opacity-0 p-4 hover:opacity-100">
             <h3 className="mt-2  text-sm lg:text-lg font-semibold text-white  dark:text-white">
               Healthcare
@@ -105,7 +105,7 @@ const Indus = () => {
           </div>
         </div>
 
-        <div className=" relative mt-1 md:h-60 lg:h-80 xl:-left-8 xl:hover:w-72 lg:left-10 lg:hover:h-80 md:left-24 sm:mt-0 sm:left-[60px] -left-10 sm:h-56 bg-black  dark:bg-zinc-800 hover:w-40 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24 xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
+        <div className=" relative xl:hover:-left-52 sm:hover:-left-24 sm:hover:w-52 mt-1 md:h-60 lg:h-80 xl:-left-8 xl:hover:w-72 lg:left-10 lg:hover:h-80 md:left-24 sm:mt-0 sm:left-[60px] left-0 sm:h-56 bg-black  dark:bg-zinc-800 hover:w-52 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24 xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
           <div className="  absolute z-10  bottom-2  opacity-0 p-4 hover:opacity-100">
             <h3 className="mt-2  text-sm lg:text-lg font-semibold text-white  dark:text-white">
               Healthcare
@@ -127,8 +127,8 @@ const Indus = () => {
           </div>
         </div>
 
-        <div className=" relative md:h-60 lg:h-80 xl:-left-20 xl:hover:w-72 lg:left-3 lg:hover:h-80 -left-20 md:left-20 sm:mt-0 sm:left-10  sm:h-56 mt-1 bg-black  dark:bg-zinc-800 hover:w-40 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24 xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
-          <div className="  absolute z-10  bottom-2  opacity-0 p-4 hover:opacity-100">
+        <div className=" relative xl:hover:-left-64 sm:hover:-left-32 sm:hover:w-60 md:h-60 lg:h-80 xl:-left-20 xl:hover:w-72 lg:left-3 lg:hover:h-80 -left-24 md:left-20 sm:mt-0 sm:left-10  sm:h-56 mt-1 bg-black  dark:bg-zinc-800 hover:w-52 w-10 sm:w-16 h-48 md:hover:h-auto md:w-16 lg:w-24 xl:w-28 md:hover:w-60 lg:hover:w-64   p-2  rounded-lg">
+          <div className="  absolute z-10   bottom-2 hover:opacity-100 opacity-0   p-4 ">
             <h3 className="mt-2  text-sm lg:text-lg font-semibold text-white  dark:text-white">
               Healthcare
             </h3>
@@ -148,6 +148,10 @@ const Indus = () => {
             </div>
           </div>
         </div>
+
+
+        
+
 
 
       </div>
@@ -156,3 +160,4 @@ const Indus = () => {
 };
 
 export default Indus;
+
