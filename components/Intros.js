19d2c0a7455f4868d5340e31e17140e0b009c26b
Sahil Sinha <115421224+SahilSinha22@users.noreply.github.com>
2024-05-22 09:36:29 +0530

    Update Intros.js

--- components/Intros.js
+++ components/Intros.js
@@ -23,7 +23,7 @@ const Intros = () => {
           with us &<br /> drive digitization to your business with innovation
           and technology
         </p>
-        <button className="border-2  border-solid rounded-full text-white font-bold py-2 px-4  hover:bg-blue-700  transition duration-300">
+        <button className="border-2  border-solid rounded-full text-white  py-2 px-4  hover:bg-blue-700  transition duration-300">
           Let's Talk!
         </button>
       </div>
@@ -42,25 +42,25 @@ const Intros = () => {
           <Image
             src={Mobi1}
             alt="App Image 2"
-            className="rounded-lg absolute h-28 top-8 sm:h-36 sm:top-4 sm:w-44 md:h-44 md:top-8 md:w-52 lg:h-56 lg:top-10 lg:w-52 xl:h-60 xl:top-8  2xl:h-60 2xl:top-16 shadow-lg transform hover:scale-105 transition duration-300"
+            className="rounded-xl absolute h-28 top-8 sm:h-36 sm:top-4 sm:w-44 md:h-44 md:top-8 md:w-52 lg:h-56 lg:top-10 lg:w-52 xl:h-60 xl:top-8  2xl:h-60 2xl:top-16 shadow-lg transform hover:scale-105 transition duration-300"
             width={100}
             height={5}
           />
           </div>
-          <div className="relative ">
+          <div className="relative sm:mx-1 ">
           <Image
             src={Mobi1}
             alt="App Image 2"
-            className="rounded-lg absolute h-24 top-12 sm:top-8 sm:h-32 sm:w-44 md:h-40 md:top-12 md:w-52 lg:h-52 lg:top-14 lg:w-52 xl:h-56 xl:top-12 2xl:h-56 2xl:top-20 shadow-lg transform hover:scale-105 transition duration-300"
+            className="rounded-xl absolute h-20 top-16 sm:top-12 sm:h-28 sm:w-44 md:h-36 md:top-16 md:w-52 lg:h-48 lg:top-[70px] lg:w-52 xl:h-52 xl:top-16 2xl:h-52 2xl:top-24 shadow-lg transform hover:scale-105 transition duration-300"
             width={100}
             height={5} 
           />
           </div>
-          <div className="relative ">
+          <div className="relative  ">
           <Image
             src={Mobi3}
             alt="App Image 3"
-            className="rounded-lg absolute h-28 top-8 sm:h-36 sm:top-4 sm:w-44 md:top-8 md:h-44 md:w-52 lg:h-56 lg:top-10 xl:h-60 xl:top-8 2xl:h-60 2xl:top-16 lg:w-52 shadow-lg transform hover:scale-105 transition duration-300"
+            className="rounded-xl absolute h-28 top-8 sm:h-36 sm:top-4 sm:w-44 md:top-8 md:h-44 md:w-52 lg:h-56 lg:top-10 xl:h-60 xl:top-8 2xl:h-60 2xl:top-16 lg:w-52 shadow-lg transform hover:scale-105 transition duration-300"
             width={100}
             height={5}
           />
@@ -69,40 +69,40 @@ const Intros = () => {
           <Image
             src={Mobi3}
             alt="App Image 4"
-            className="rounded-lg  absolute h-36 sm:h-52 sm:-top-10 sm:w-44 md:w-52 md:-top-7 md:h-60 2xl:-top-5 xl:-top-10 lg:h-72 lg:-top-5 lg:w-52 xl:h-80 2xl:h-80 shadow-lg transform hover:scale-105 transition duration-300"
+            className="rounded-xl  absolute h-36 sm:h-52 sm:-top-10 sm:w-44 md:w-52 md:-top-7 md:h-60 2xl:-top-5 xl:-top-10 lg:h-72 lg:-top-5 lg:w-52 xl:h-80 2xl:h-80 shadow-lg transform hover:scale-105 transition duration-300"
             width={100}
             height={5}
           />
           </div>
         </div>
       </div>
-      <div className="grid md:px-20 lg:px-12 2xl:px-56 grid-cols-4 h-20 xl:px-40 sm:h-24  sm:px-10 sm:grid-cols-4 px-8 md:flex mt-5 justify-center md:justify-around  md:space-x-4 md:mt-40 lg:mt-8 bg-purple-500 md:h-32 xl:h-56">
+      <div className="grid md:px-24 lg:px-12 2xl:px-56 grid-cols-4 h-20 xl:px-40 sm:h-32  sm:px-10 sm:grid-cols-4 px-8 md:flex mt-5 justify-center md:justify-around  md:space-x-4 md:mt-40 lg:mt-8 bg-purple-500 md:h-32 xl:h-56">
         <Image
           src={Mobi2}
           alt="Company Logo 1"
-          className=" md:h-40 lg:w-28 sm:w-20 xl:w-40 xl:h-60 md:w-20 md:mx-5 mt-8 sm:mt-0 xl:mt-8  md:mt-0  md:pt-0 sm:pt-5 opacity-75 hover:opacity-100 transition duration-300"
-          width={50}
+          className=" md:h-40 lg:w-28 sm:w-28 xl:w-40 xl:h-60 md:w-28 md:mx-5 mt-3 sm:mt-1 xl:mt-8  md:mt-0  md:pt-0 sm:pt-5 opacity-75 hover:opacity-100 transition duration-300"
+          width={80}
           height={5}
         />
         <Image
           src={Mobi2}
           alt="Company Logo 2"
-          className="md:h-40 lg:w-28 xl:w-40 sm:w-20 xl:h-60 md:w-20 md:mx-5 mt-8 sm:mt-0 xl:mt-8 md:mt-0  md:pt-0 sm:pt-5 opacity-75 hover:opacity-100 transition duration-300"
-          width={50}
+          className="md:h-40 lg:w-28 xl:w-40 sm:w-28 xl:h-60 md:w-28 md:mx-5 mt-3 sm:mt-1 xl:mt-8 md:mt-0  md:pt-0 sm:pt-5 opacity-75 hover:opacity-100 transition duration-300"
+          width={80}
           height={5}
         />
         <Image
           src={Mobi}
           alt="Company Logo 3"
-          className="md:h-40 lg:w-28 xl:w-40 sm:w-20 xl:h-60 md:w-20 md:mx-5 mt-8 sm:mt-0 xl:mt-8 md:mt-0  md:pt-0 sm:pt-5 opacity-75 hover:opacity-100 transition duration-300"
-          width={50}
+          className="md:h-40 lg:w-28 xl:w-40 sm:w-32 xl:h-60 md:w-28 md:mx-5 mt-3 sm:mt-0 xl:mt-8 md:mt-0  md:pt-0 sm:pt-5 opacity-75 hover:opacity-100 transition duration-300"
+          width={80}
           height={5}
         />
         <Image
           src={Mobi}
           alt="Company Logo 4"
-          className="md:h-40 lg:w-28 xl:w-40 sm:w-20 xl:h-60 md:w-20 md:mx-5 mt-8 sm:mt-0 xl:mt-8 md:mt-0  md:pt-0 sm:pt-5 opacity-75 hover:opacity-100 transition duration-300"
-          width={50}
+          className="md:h-40 lg:w-28 xl:w-40 sm:w-32 xl:h-60 md:w-28 md:mx-5 mt-3 sm:mt-0 xl:mt-8 md:mt-0  md:pt-0 sm:pt-5 opacity-75 hover:opacity-100 transition duration-300"
+          width={80}
           height={5}
         />
       </div>
