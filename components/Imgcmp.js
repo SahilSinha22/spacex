ef53ed3956ab5f7750423db4c1091d11b7a412ba
SahilSinha22 <sahilsinha2204@gmail.com>
2024-06-04 10:43:18 +0530

    comp4

--- components/Imgcmp.js
+++ components/Imgcmp.js
@@ -11,8 +11,8 @@ import mobi8 from "@/public/8imgcmp.png";
 const Imgcmp = () => {
   return (
     <div className="mx-4 md:mx-8 xl:mx-36 grid  mb-10 items-center justify-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2">
-      <div className="hover:bg-zinc-800   bg-black xl:w-64 2xl:w-80 h-44 px-6 rounded-xl space-y-2">
-      <div className="text-zinc-400 mt-2 flex flex-row justify-between">
+      <div className="hover:bg-zinc-800  imgco bg-black  2xl:w-80 h-44 px-6 xl:mr-10 rounded-xl space-y-2">
+        <div className="text-zinc-400 mt-2 flex flex-row justify-between">
           <div className="">01</div>
           <div className=" opacity-0 hover:opacity-100">
             <Image
@@ -24,28 +24,27 @@ const Imgcmp = () => {
             />{" "}
           </div>
         </div>
-        
-          <h2 className="text-xl text-zinc-500 font-poppins xl:text-2xl ">
-            Blockchain
-          </h2>
-          <p className="text-zinc-500">
-            Our team is experienced <br />
-            with various
-          </p>
-          <div className="text-blue-300 cursor-pointer relative right-2 bottom-7  items-right">
-            <Image
-              className="absolute lg:right-1 xl:-bottom-8  right-2"
-              src={Arrow}
-              alt="Arrow"
-              width={20}
-              height={5}
-            />
-          </div>
-        
+
+        <h2 className="text-xl text-zinc-500 font-poppins xl:text-2xl ">
+          Blockchain
+        </h2>
+        <p className="text-zinc-500">
+          Our team is experienced <br />
+          with various
+        </p>
+        <div className="text-blue-300 cursor-pointer relative right-2 bottom-7  items-right">
+          <Image
+            className="absolute lg:right-1 xl:-bottom-6 xl:-right-4 right-2"
+            src={Arrow}
+            alt="Arrow"
+            width={20}
+            height={5}
+          />
+        </div>
       </div>
 
-      <div className="hover:bg-zinc-800  bg-black xl:w-64 2xl:w-80 h-44 px-6 rounded-xl space-y-2">
-      <div className="text-zinc-400 mt-2 flex flex-row justify-between">
+      <div className="hover:bg-zinc-800  imgco  bg-black  2xl:w-80 h-44 px-6 rounded-xl space-y-2">
+        <div className="text-zinc-400 mt-2 flex flex-row justify-between">
           <div className="">02</div>
           <div className=" opacity-0 hover:opacity-100">
             <Image
@@ -57,26 +56,25 @@ const Imgcmp = () => {
             />{" "}
           </div>
         </div>
-       
-          <h2 className="text-xl text-zinc-500 xl:text-2xl">AR/VR</h2>
-          <p className="text-zinc-500">
-            Our team is experienced <br />
-            with various
-          </p>
-          <div className="text-blue-300 cursor-pointer relative  bottom-7">
-            <Image
-              className="absolute xl:-bottom-8 right-2"
-              src={Arrow}
-              alt="Arrow"
-              width={20}
-              height={5}
-            />
-          </div>
-        </div>
-     
 
-      <div className="hover:bg-zinc-800  bg-black xl:w-64 2xl:w-80 h-44 px-6 rounded-xl space-y-2">
-      <div className="text-zinc-400 mt-2 flex flex-row justify-between">
+        <h2 className="text-xl text-zinc-500 xl:text-2xl">AR/VR</h2>
+        <p className="text-zinc-500">
+          Our team is experienced <br />
+          with various
+        </p>
+        <div className="text-blue-300 cursor-pointer relative  bottom-7">
+          <Image
+            className="absolute xl:-bottom-6 xl:-right-4 right-2"
+            src={Arrow}
+            alt="Arrow"
+            width={20}
+            height={5}
+          />
+        </div>
+      </div>
+
+      <div className="hover:bg-zinc-800 imgco bg-black  2xl:w-80 h-44 px-6 rounded-xl space-y-2">
+        <div className="text-zinc-400 mt-2 flex flex-row justify-between">
           <div className="">03</div>
           <div className=" opacity-0 hover:opacity-100">
             <Image
@@ -88,26 +86,25 @@ const Imgcmp = () => {
             />{" "}
           </div>
         </div>
-        
-          <h2 className="text-xl text-zinc-500 xl:text-2xl">Metaverse</h2>
-          <p className="text-zinc-500">
-            Our team is experienced <br />
-            with various
-          </p>
-          <div className="text-blue-300 cursor-pointer relative right-1 bottom-7">
-            <Image
-              className="absolute xl:-bottom-8 right-2"
-              src={Arrow}
-              alt="Arrow"
-              width={20}
-              height={5}
-            />
-          </div>
-        
+
+        <h2 className="text-xl text-zinc-500 xl:text-2xl">Metaverse</h2>
+        <p className="text-zinc-500">
+          Our team is experienced <br />
+          with various
+        </p>
+        <div className="text-blue-300 cursor-pointer relative right-1 bottom-7">
+          <Image
+            className="absolute xl:-bottom-6 xl:-right-4 right-2"
+            src={Arrow}
+            alt="Arrow"
+            width={20}
+            height={5}
+          />
+        </div>
       </div>
 
-      <div className="hover:bg-zinc-800  bg-black px-6 xl:w-64 2xl:w-80 h-44 rounded-xl space-y-2">
-      <div className="text-zinc-400 mt-2 flex flex-row justify-between">
+      <div className="hover:bg-zinc-800 imgco bg-black px-6  2xl:w-80 h-44 rounded-xl space-y-2">
+        <div className="text-zinc-400 mt-2 flex flex-row justify-between">
           <div className="">04</div>
           <div className=" opacity-0 hover:opacity-100">
             <Image
@@ -119,26 +116,25 @@ const Imgcmp = () => {
             />{" "}
           </div>
         </div>
-       
-          <h2 className="text-xl text-zinc-500 xl:text-2xl">IOT</h2>
-          <p className="text-zinc-500">
-            Our team is experienced <br />
-            with various
-          </p>
-          <div className="text-blue-300 cursor-pointer relative right-2 bottom-7">
-            <Image
-              className="absolute xl:-bottom-8 right-2"
-              src={Arrow}
-              alt="Arrow"
-              width={20}
-              height={5}
-            />
-          </div>
-        </div>
-      
 
-      <div className="hover:bg-zinc-800  bg-black px-6 xl:w-64 2xl:w-80 h-44 rounded-xl space-y-2">
-      <div className="text-zinc-400 mt-2 flex flex-row justify-between">
+        <h2 className="text-xl text-zinc-500 xl:text-2xl">IOT</h2>
+        <p className="text-zinc-500">
+          Our team is experienced <br />
+          with various
+        </p>
+        <div className="text-blue-300 cursor-pointer relative right-2 bottom-7">
+          <Image
+            className="absolute xl:-bottom-6 xl:-right-4 right-2"
+            src={Arrow}
+            alt="Arrow"
+            width={20}
+            height={5}
+          />
+        </div>
+      </div>
+
+      <div className="hover:bg-zinc-800 imgco bg-black px-6  2xl:w-80 h-44 rounded-xl space-y-2">
+        <div className="text-zinc-400 mt-2 flex flex-row justify-between">
           <div className="">05</div>
           <div className=" opacity-0 hover:opacity-100">
             <Image
@@ -150,26 +146,25 @@ const Imgcmp = () => {
             />{" "}
           </div>
         </div>
-       
-          <h2 className="text-xl text-zinc-500 xl:text-2xl">NFT</h2>
-          <p className="text-zinc-500">
-            Our team is experienced <br />
-            with various
-          </p>
-          <div className="text-blue-300 cursor-pointer relative right-2 bottom-7">
-            <Image
-              className="absolute xl:-bottom-8 right-2"
-              src={Arrow}
-              alt="Arrow"
-              width={20}
-              height={5}
-            />
-          </div>
-        </div>
-      
 
-      <div className="hover:bg-zinc-800  bg-black px-6 xl:w-64 2xl:w-80 h-44 rounded-xl space-y-2">
-      <div className="text-zinc-400 mt-2 flex flex-row justify-between">
+        <h2 className="text-xl text-zinc-500 xl:text-2xl">NFT</h2>
+        <p className="text-zinc-500">
+          Our team is experienced <br />
+          with various
+        </p>
+        <div className="text-blue-300 cursor-pointer relative right-2 bottom-7">
+          <Image
+            className="absolute xl:-bottom-6 xl:-right-4 right-2"
+            src={Arrow}
+            alt="Arrow"
+            width={20}
+            height={5}
+          />
+        </div>
+      </div>
+
+      <div className="hover:bg-zinc-800 imgco bg-black px-6  2xl:w-80 h-44 rounded-xl space-y-2">
+        <div className="text-zinc-400 mt-2 flex flex-row justify-between">
           <div className="">06</div>
           <div className=" opacity-0 hover:opacity-100">
             <Image
@@ -181,25 +176,24 @@ const Imgcmp = () => {
             />{" "}
           </div>
         </div>
-        
-          <h2 className="text-xl text-zinc-500 xl:text-2xl">AI Development</h2>
-          <p className="text-zinc-500">
-            Our team is experienced <br />
-            with various
-          </p>
-          <div className="text-blue-300 cursor-pointer relative right-2 bottom-7">
-            <Image
-              className="absolute xl:-bottom-8 right-2"
-              src={Arrow}
-              alt="Arrow"
-              width={20}
-              height={5}
-            />
-          </div>
-       
+
+        <h2 className="text-xl text-zinc-500 xl:text-2xl">AI Development</h2>
+        <p className="text-zinc-500">
+          Our team is experienced <br />
+          with various
+        </p>
+        <div className="text-blue-300 cursor-pointer relative right-2 bottom-7">
+          <Image
+            className="absolute xl:-bottom-6 xl:-right-4 right-2"
+            src={Arrow}
+            alt="Arrow"
+            width={20}
+            height={5}
+          />
+        </div>
       </div>
 
-      <div className="hover:bg-zinc-800  bg-black px-6 xl:w-64 2xl:w-80 h-44 rounded-xl space-y-2">
+      <div className="hover:bg-zinc-800 imgco  bg-black px-6  2xl:w-80 h-44 rounded-xl space-y-2">
         <div className="text-zinc-400 mt-2 flex flex-row justify-between">
           <div className="">07</div>
           <div className=" opacity-0 hover:opacity-100">
@@ -220,7 +214,7 @@ const Imgcmp = () => {
         </p>
         <div className="text-blue-300 cursor-pointer relative right-2 bottom-7">
           <Image
-            className="absolute xl:-bottom-6 right-2"
+            className="absolute xl:-bottom-6 xl:-right-4 right-2"
             src={Arrow}
             alt="Arrow"
             width={20}
@@ -229,8 +223,8 @@ const Imgcmp = () => {
         </div>
       </div>
 
-      <div className="hover:bg-zinc-800  bg-black px-6 xl:w-64 2xl:w-80 h-44 rounded-xl space-y-2">
-      <div className="text-zinc-400 mt-2 flex flex-row justify-between">
+      <div className="hover:bg-zinc-800 imgco bg-black px-6  2xl:w-80 h-44 rounded-xl space-y-2">
+        <div className="text-zinc-400 mt-2 flex flex-row justify-between">
           <div className="">08</div>
           <div className=" opacity-0 hover:opacity-100">
             <Image
@@ -242,24 +236,23 @@ const Imgcmp = () => {
             />{" "}
           </div>
         </div>
-        
-          <h2 className="text-xl text-zinc-500 xl:text-2xl">iOS</h2>
-          <p className="text-zinc-500">
-            Our team is experienced <br />
-            with various
-          </p>
-          <div className="text-blue-300 cursor-pointer relative right-2 bottom-7">
-            <Image
-              className="absolute xl:-bottom-8 right-2"
-              src={Arrow}
-              alt="Arrow"
-              width={20}
-              height={5}
-            />
-          </div>
+
+        <h2 className="text-xl text-zinc-500 xl:text-2xl">iOS</h2>
+        <p className="text-zinc-500">
+          Our team is experienced <br />
+          with various
+        </p>
+        <div className="text-blue-300 cursor-pointer relative right-2 bottom-7">
+          <Image
+            className="absolute xl:-bottom-6 xl:-right-4 right-2"
+            src={Arrow}
+            alt="Arrow"
+            width={20}
+            height={5}
+          />
         </div>
       </div>
-    
+    </div>
   );
 };
 
