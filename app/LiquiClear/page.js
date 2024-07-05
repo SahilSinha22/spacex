f4a7eacf7ade5164ffce2acb66c92cb252011234
SahilSinha22 <sahilsinha2204@gmail.com>
2024-07-05 10:44:51 +0530

    comp790

--- app/LiquiClear/page.js
+++ app/LiquiClear/page.js
@@ -1,7 +1,11 @@
-
-   "use client";
+"use client";
 import React from "react";
 import Mobi from "@/public/Liqiclear.svg";
+import Mobi1 from "@/public/water.png";
+import Mobi2 from "@/public/drop.png";
+import Mobi3 from "@/public/plant.png";
+import Mobi4 from "@/public/sec.png"
+
 
 import Mobi5 from "@/public/screen.png";
 import Mobi6 from "@/public/mobile.png";
@@ -41,10 +45,11 @@ const page = () => {
         </div>
         <div className="h-12 md:h-32 xl:h-44"></div>
         <div class="relative bg-[#005AA8]  h-auto py-8">
+
           <div class=" absolute  -top-20 md:-top-40 xl:-top-52  px-4 sm:px-6 lg:px-32">
             <div class=" grid grid-cols-3 md:grid-cols-3  gap-8 lg:gap-28 items-center">
               <div class="text-center ">
-                
+                <Image src={Mobi1} class="w-full rounded-2xl h-68  object-cover " alt="glass of water" width={300} height={10}/>
                 <h3 class="mt-4 text-white text-xl xl:text-2xl font-semibold">
                   Maximum Purity
                 </h3>
@@ -55,7 +60,8 @@ const page = () => {
               </div>
 
               <div class="text-center">
-               
+              <Image src={Mobi2} class="w-full rounded-2xl h-68  object-cover " alt="glass of water" width={300} height={10}/>
+
                 <h3 class="mt-4 text-white text-xl xl:text-2xl font-semibold">
                   Energy Efficient
                 </h3>
@@ -66,7 +72,8 @@ const page = () => {
               </div>
 
               <div class="text-center items-center">
-                
+              <Image src={Mobi3} class="w-full rounded-2xl h-68  object-cover " alt="glass of water" width={300} height={10}/>
+
                 <h3 class="mt-4 text-white text-xl xl:text-2xl font-semibold">
                   Cost Effective
                 </h3>
@@ -78,7 +85,15 @@ const page = () => {
               </div>
             </div>
           </div>
-          
+          <div class="text-center mt-72 md:pt-20 mx-10 xl:mx-32 xl:mt-60 xl:pt-40 items-center">
+          <Image
+            class="w-full rounded-2xl h-68 object-cover "
+            src={Mobi4}
+            alt="Plant growing in coins jar"
+            width={1000}
+            height={10}
+          />
+        </div>
         </div>
         
 
