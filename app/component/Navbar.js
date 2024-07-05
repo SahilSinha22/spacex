d934ad83cec03ac1c18a854385471e25e03c579e
SahilSinha22 <sahilsinha2204@gmail.com>
2024-07-05 10:00:11 +0530

    comp790

--- app/component/Navbar.js
+++ app/component/Navbar.js
@@ -3,13 +3,14 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/public/logo.png"
+
 import { usePathname } from "next/navigation";
 
 const logos = {
   '/': 'logo.png',
   '/Portfolio': 'logo1.png',
   '/LiquiClear': 'logo1.png',
+  '/Services':'logo1.png',
   
 };
 
