3dda9e4c27254a4a85e9af0d02f61ba627883f22
sahil sinha <sahilsinha2204@gmail.com>
2024-08-09 13:06:17 +0530

    Indus change

--- components/Indus.js
+++ components/Indus.js
@@ -7,31 +7,29 @@ const Indus = () => {
   useEffect(() => {
     console.log("hello");
     const panels = document.querySelectorAll(".panel");
-    
+
     panels.forEach((panel) => {
-      panel.addEventListener("click", () => {
-        removeActiveClasses();
-        panel.classList.add("active");
-      });
+      panel.addEventListener("mouseover", handleMouseEnter);
     });
+
+    function handleMouseEnter(event) {
+      removeActiveClasses();
+      event.currentTarget.classList.add("active");
+    }
 
     function removeActiveClasses() {
       panels.forEach((panel) => {
         panel.classList.remove("active");
       });
     }
-    console.log("hellow1");
 
     // Cleanup event listeners on component unmount
     return () => {
       panels.forEach((panel) => {
-        panel.removeEventListener("click", () => {
-          removeActiveClasses();
-          panel.classList.add("active");
-        });
+        panel.removeEventListener("mouseover", handleMouseEnter);
       });
     };
-  }, []); // Empty dependency array means this runs once after initial render
+  }, []);  // Empty dependency array means this runs once after initial render
 
   return (
     <div className=" max-w-7xl mx-10  lg:px-8 py-12">
