dcf1c90285823337617802726841d302e2df754c
sahil sinha <sahilsinha2204@gmail.com>
2024-08-23 12:00:07 +0530

    cont

--- components/Flutter.jsx
+++ components/Flutter.jsx
@@ -63,11 +63,11 @@ const Flutter = () => {
         <>
             <div className='mx-auto'>
                 <div class="max-w-7xl items-center text-center mx-auto px-2 md:px-10 xl:px-20 2xl:px-10 py-16 md:py-20 lg:pt-28 lg:pb-10 ">
-                    <h1 class="text-xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl md:leading-snug xl:leading-normal   font-bold playfair_displays mb-4">Flutter App Development Company Helps <br/>You Build Applications for Multiple Screens!</h1>
+                    <h1 class="text-xl sm:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl md:leading-snug xl:leading-normal   font-bold playfair_displays mb-4">Flutter App Development Company Helps <br/>You Build Applications for Multiple Screens!</h1>
                     <p class="  lg:text-xl xl:text-2xl   text-muted-foreground mb-6">Introducing big waves in modern cross-platform app development, Flutter tech <br /> builds exquisite, high-performing, and flawless mobile apps.</p>
 
                 </div>
-                <div className="flex relative flex-wrap justify-center gap-4 px-2 md:px-10 max-w-7xl mx-auto">
+                <div className="flex relative flex-wrap justify-center gap-4 px-2 sm:px-10 max-w-7xl mx-auto">
     <video
         autoPlay
         muted
@@ -90,7 +90,7 @@ const Flutter = () => {
 </div>
  
 
-                <div class=" max-w-7xl mx-auto my-10 mt-40 open_sans_display px-2 md:px-10 xl:px-20 ">
+                <div class=" max-w-7xl mx-auto my-10 mt-40 open_sans_display px-2 sm:px-10 xl:px-20 ">
                     <h1 class="text-3xl md:text-5xl  text-primary mb-4">Introduction to <span class="text-blue-500">Flutter</span></h1>
                     <p class=" text-xl md:text-2xl leading-relaxed  mb-1">
                         Initially released by Google in 2017, it is an app framework rapidly amplified with yearly improvements and alterations. The tech is well-known for providing charismatic, distinctive, and natively
@@ -143,30 +143,30 @@ const Flutter = () => {
                     </div>
                 </div>
 
-                <div class=" bg-background max-w-7xl mx-auto pt-10 text-foreground px-2 md:px-10 xl:px-20 ">
+                <div class=" bg-background max-w-7xl mx-auto pt-10 text-foreground px-2 sm:px-10 xl:px-20 ">
                     <h1 class="text-2xl md:text-4xl  mb-4">Trump Card Played By Flutter App Development Company</h1>
                     <p class="mb-6 md:text-xl leading-normal">The flutter mechanism comes along with a number of benefits, and the best flutter <br />app development company in India, UK & USA helps you bag the same.</p>
                     <div class="grid gap-4 md:grid-cols-2 open_sans_display">
-                        <div class="p-4 border border-border open_sans_display  boxes bg-[#F8F8F8]">
+                        <div class="p-4 border border-border open_sans_display  cont boxes bg-[#F8F8F8]">
                             <h2 class="text-xl open_sans_display pt-1 pb-1 font-semibold">Reduced Time for App Development</h2>
                             <p>
                                 When working on a medium-sized Android application, the process of adjusting a layout feature can consume up to 40 seconds. However, with the built-in hot reload feature, your modifications
                                 become nearly instantaneous.
                             </p>
                         </div>
-                        <div class="p-4 border border-border boxes bg-[#F8F8F8]">
+                        <div class="p-4 border cont border-border boxes bg-[#F8F8F8]">
                             <h2 class="text-xl open_sans_display pt-1 pb-1 font-semibold">Accelerated Time To Market</h2>
                             <p>
                                 Building apps separately for different platforms will require varied specialized teams. However, it takes only half a workforce of a Flutter app development company to work with the framework, as platform-specific code is not required.
                             </p>
                         </div>
-                        <div class="p-4 border border-border  boxes bg-[#F8F8F8]">
+                        <div class="p-4 cont border border-border  boxes bg-[#F8F8F8]">
                             <h2 class="text-xl open_sans_display pt-1 pb-1 font-semibold">Similar Framework to Native App Development</h2>
                             <p>
                                 Building digital products also demands fabricating apps that prioritize the user experience. Therefore, the Flutter framework can create exceptional user interface animations and eliminate performance bugs.
                             </p>
                         </div>
-                        <div class="p-4 border border-border  boxes bg-[#F8F8F8]">
+                        <div class="p-4 cont border border-border  boxes bg-[#F8F8F8]">
                             <h2 class="text-xl open_sans_display pt-1 pb-1 font-semibold">Speedy App Growth</h2>
                             <p>
                                 You can accelerate both development and expansion by utilizing numerous widgets in your development process. Additionally, if you aim to capture a share of the market for your application,
@@ -174,7 +174,7 @@ const Flutter = () => {
                             </p>
                         </div>
 
-                        <div class="p-4 border border-border   boxes bg-[#F8F8F8]">
+                        <div class="p-4 border border-border cont  boxes bg-[#F8F8F8]">
                             <h2 class="text-xl open_sans_display pt-1 pb-1 font-semibold">Minimalist Design Features</h2>
                             <p>
                                 Should you desire to employ unique widgets within your application, Flutter can fashion new ones. These newly crafted widgets can be utilized independently or seamlessly with pre-existing
@@ -187,7 +187,7 @@ const Flutter = () => {
                 <div className=' my-16 boxshad open_sans_display p-8'>
 
 
-                    <div class="max-w-7xl mx-auto md:px-6 xl:px-20 ">
+                    <div class="max-w-7xl mx-auto sm:px-6 xl:px-20 ">
                         <h2 class="text-2xl md:text-3xl font-bold text-white mb-4">Types of Businesses That Prefer Using Flutter</h2>
                         <p class="md:text-lg text-white mb-6">
                             While Flutter seems like a potential framework, there may be better fits for some projects. Can you leverage the framework in your app with the help of a Flutter mobile app development company?
@@ -252,7 +252,7 @@ const Flutter = () => {
                     </div>
                 </div>
 
-                <div class="max-w-7xl mx-auto  bg-background text-foreground open-sans_display px-2 md:px-10 xl:px-20  ">
+                <div class="max-w-7xl mx-auto  bg-background text-foreground open-sans_display px-2 sm:px-10 xl:px-20  ">
                     <h1 class="text-2xl md:text-3xl font-bold mb-4">Space to Tech : Your One-Stop-Shop To Success</h1>
                     <p class="mb-6  xl:text-xl ">
                         Evidently, you’re planning to build a mobile app to push your business ahead of the competition.<br /> Must be giddy with enthusiasm? But have you figured out the way to get kickstarted?
@@ -307,12 +307,12 @@ const Flutter = () => {
                 <div className='bg-[#5F2AE2] mt-20 xl:px-20'>
 
 
-                    <div className=' md:px-10 mt-10 flex  py-10 max-w-7xl gap-4 md:gap-16 mx-auto'>
+                    <div className=' sm:px-10 mt-10 flex  py-10 max-w-7xl gap-4 md:gap-16 mx-auto'>
                         <div>
-                            <Image src="/phoni.png" className='w-40 md:w-60 lg:w-[400px] h-full' alt="" width={1000} height={1000} />
+                            <Image src="/phoni.png" className='w-40 sm:w-60 lg:w-[400px] h-full' alt="" width={1000} height={1000} />
                         </div>
                         <div className='place-content-center'>
-                            <h2 className='text-xl md:text-4xl lg:text-5xl 2xl:text-6xl leading-snug lg:leading-normal xl:leading-snug open_sans_displays font-light text-white '> What is Flutter App <br /> Development?</h2>
+                            <h2 className='text-xl sm:text-4xl lg:text-5xl 2xl:text-6xl leading-snug lg:leading-normal xl:leading-snug open_sans_displays font-light text-white '> What is Flutter App <br /> Development?</h2>
                             <button onClick={handleClick} class="text-white mt-4  max-w-xl text-xs md:text-sm lg:text-base xl:text-lg bg-gradient-to-r from-[#C9784F] via-[#A06A7B] to-[#6C506F] px-3  md:px-6 py-2 rounded-3xl">Let's Talk</button>
 
                         </div>
@@ -322,8 +322,8 @@ const Flutter = () => {
                 <div className='2xl:px-40 '>
                     <Indus />
                 </div>
-                <div className="bg-black h-auto pt-2  md:px-10 xl:px-20">
-                    <div className="bg-black text-white open_sans_displays   max-w-7xl mx-auto text-foreground p-6">
+                <div className="bg-black h-auto pt-2  sm:px-10 xl:px-20">
+                    <div className="bg-black text-white open_sans_displays pt-6  max-w-7xl mx-auto text-foreground p-6">
                         <Image aria-hidden="true" alt="FAQ Icon" src="/FAQ.svg" className="mr-2" width={60} height={60} />
                         <div className="space-y-4 my-10 xl:my-20">
                             {faqContent.map((item, index) => (
